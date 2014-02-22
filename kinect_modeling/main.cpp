befcae79031a9df7517a6799a47fb037d255af47
Bryant Pong <rcxking@yahoo.com>
2014-02-22 17:20:48 -0500

    Added Initial Kinect Code

---
+++ kinect_modeling/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * main.cpp - Main Kinect Code to generate 3-Dimensional models of people for
+ * Project Doll.
+ *
+ * Bryant Pong
+ * 2/22/14
+ * Software Design and Documentation
+ *
+ * Last Updated: Bryant Pong 2/22/14 - 4:21 PM
+ */
+ 
+// OpenNI Header:
+#include "OpenNI.h"  
+
+// C++ STL Libraries:
+#include <iostream>
+
+// Main function:
+int main(int argc, char **argv) {
+	return 0;
+} // End function main
