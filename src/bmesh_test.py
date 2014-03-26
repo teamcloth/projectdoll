43446e1e226dabbd4a0318b8a79fcedbfa94d7a6
Bryant Pong <rcxking@yahoo.com>
2014-03-26 15:51:59 -0400

    Continued work on bmesh_test.py

--- src/bmesh_test.py
+++ src/bmesh_test.py
@@ -4,13 +4,13 @@ import bpy
 import bmesh
 
 # Get the active mesh
-me = bpy.context.object.data
-
+me = bpy.data.meshes['GZM_Shoulder.001']
 
 # Get a BMesh representation
 bm = bmesh.new()   # create an empty BMesh
 bm.from_mesh(me)   # fill it in from a Mesh
-bm.verts[0].co.x += 1.0
+for i in bm.verts:
+    i.co.x -= 100
 
 
 # Finish up, write the bmesh back to the mesh
