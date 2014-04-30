6f6aa0ab3a6e95ddd0665a7524f67db4a4abce9f
Bryant Pong <rcxking@yahoo.com>
2014-04-30 16:16:16 -0400

    Removed debug statements

--- ProjectDoll/install.py
+++ ProjectDoll/install.py
@@ -6,7 +6,7 @@ install.py - Python script to install Project doll into a new user's directory.
 Team Cloth
 4/30/13
 
-Last Updated: 4/30/13 - 2:48 PM
+Last Updated: 4/30/13 - 4:15 PM
 '''
 import os
 import shutil
@@ -17,7 +17,6 @@ def main():
 	# Get the path to Blender:
 	blenderPath = str(raw_input("Please enter the path to Blender: "))
 	blenderPath += "/2.69/scripts/"
-	print("DEBUG ONLY - blenderPath is: " + blenderPath) 
 	
 	'''
 	We need the following path variables to move the Project Doll files:
@@ -31,25 +30,16 @@ def main():
 	blenderAddons = blenderPath + "addons/"
 	blenderModules = blenderPath + "modules/"
 
-	print("DEBUG ONLY - currentDirectory: " + currentDirectory)
-	print("DEBUG ONLY - blenderAddons: " + blenderAddons)
-	print("DEBUG ONLY - blenderModules: " + blenderModules)
-
 	# Does the ProjectDoll directory exist in blenderAddons?     	
 	if not os.path.exists(blenderAddons + "ProjectDoll"):
-		print("DEBUG ONLY - ProjectDoll does not exist in blenderAddons")
 		os.makedirs(blenderAddons + "ProjectDoll")
-	else:
-		print("DEBUG ONLY - ProjectDoll exists in blenderAddons")
 
 	# Copy __init__.py into the "ProjectDoll" directory in addons:
 	initFilePath = currentDirectory + "/__init__.py"
-	print("DEBUG ONLY - initFilePath is: " + initFilePath)
 	shutil.copy(initFilePath, blenderAddons + "ProjectDoll")
 
 	# Copy all files in the modules directory:
 	modulesDirectory = currentDirectory + "/modules"
-	print("DEBUG ONLY - modulesDirectory is: " + modulesDirectory)	 
 	
 	# Get all files in the modulesDirectory:
 	modulesFiles = os.listdir(modulesDirectory)
