12d254473e9790c29885fc52008cb284532f6ace
Bryant Pong <rcxking@yahoo.com>
2014-04-30 15:10:32 -0400

    Installation script works on Ubuntu

--- ProjectDoll/install.py
+++ ProjectDoll/install.py
@@ -6,7 +6,7 @@ install.py - Python script to install Project doll into a new user's directory.
 Team Cloth
 4/30/13
 
-Last Updated: 4/30/13 - 2:15 PM
+Last Updated: 4/30/13 - 2:48 PM
 '''
 import os
 import shutil
@@ -22,13 +22,16 @@ def main():
 	'''
 	We need the following path variables to move the Project Doll files:
 	
+	currentDirectory: Current directory. 
 	blenderAddons: Directory that holds the __init__.py file.
 	blenderModules: Directory that holds the files in "Modules".
 	'''
 
+	currentDirectory = os.getcwd()
 	blenderAddons = blenderPath + "addons/"
 	blenderModules = blenderPath + "modules/"
 
+	print("DEBUG ONLY - currentDirectory: " + currentDirectory)
 	print("DEBUG ONLY - blenderAddons: " + blenderAddons)
 	print("DEBUG ONLY - blenderModules: " + blenderModules)
 
@@ -38,6 +41,24 @@ def main():
 		os.makedirs(blenderAddons + "ProjectDoll")
 	else:
 		print("DEBUG ONLY - ProjectDoll exists in blenderAddons")
+
+	# Copy __init__.py into the "ProjectDoll" directory in addons:
+	initFilePath = currentDirectory + "/__init__.py"
+	print("DEBUG ONLY - initFilePath is: " + initFilePath)
+	shutil.copy(initFilePath, blenderAddons + "ProjectDoll")
+
+	# Copy all files in the modules directory:
+	modulesDirectory = currentDirectory + "/modules"
+	print("DEBUG ONLY - modulesDirectory is: " + modulesDirectory)	 
+	
+	# Get all files in the modulesDirectory:
+	modulesFiles = os.listdir(modulesDirectory)
+	print(modulesFiles)
+
+	for nextFile in modulesFiles:
+		fullFileName = currentDirectory + "/modules/" + nextFile
+		shutil.copy(fullFileName, blenderModules)	 
+	  	
 
 # Run the installation script:
 if __name__ == "__main__":
